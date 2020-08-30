11bffec0f7149dda25022d584b43fc4572673921
daiwk <daiwk@foxmail.com>
2020-08-30 23:58:27 +0800

    x

--- gen_pdfs.sh
+++ gen_pdfs.sh
@@ -1,5 +1,5 @@
 cat ./posts/*.md | python3 trans_format.py > ./x.md
-pandoc -N -s --toc --smart --latex-engine=xelatex -V CJKmainfont='Heiti SC' -V mainfont='Times New Roman' -V geometry:margin=1in -f markdown+markdown_in_html_blocks+raw_html-implicit_figures ./x.md -o ./pdfs/collections-all.pdf
+#pandoc -N -s --toc --smart --latex-engine=xelatex -V CJKmainfont='Heiti SC' -V mainfont='Times New Roman' -V geometry:margin=1in -f markdown+markdown_in_html_blocks+raw_html-implicit_figures ./x.md -o ./pdfs/collections-all.pdf
 
 git add posts
 git add pdfs
