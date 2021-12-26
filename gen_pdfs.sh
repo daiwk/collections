43b8bcd4f75dc4f89c505d8f70a5e5b368e3d2f9
daiwk <daiwk@foxmail.com>
2021-12-27 05:24:39 +0800

    x

--- gen_pdfs.sh
+++ gen_pdfs.sh
@@ -1,6 +1,7 @@
 cat ./posts/*.md | python3 trans_format.py > ./x.md
 #pandoc -N -s --toc --smart --latex-engine=xelatex -V CJKmainfont='Heiti SC' -V mainfont='Times New Roman' -V geometry:margin=1in -f markdown+markdown_in_html_blocks+raw_html-implicit_figures ./x.md -o ./pdfs/collections-all.pdf
 
+git add ./assets
 git add collections.md
 git add posts
 git add pdfs
