85480bf4bacab789410b3c57cdc0dd0043e1d8ad
daiwk <daiwk@foxmail.com>
2022-05-31 05:15:02 +0800

    x

--- gen_pdfs.sh
+++ gen_pdfs.sh
@@ -1,10 +1,14 @@
-cat ./posts/*.md | python3 trans_format.py > ./collections.md
-#pandoc -N -s --toc --smart --latex-engine=xelatex -V CJKmainfont='Heiti SC' -V mainfont='Times New Roman' -V geometry:margin=1in -f markdown+markdown_in_html_blocks+raw_html-implicit_figures ./x.md -o ./pdfs/collections-all.pdf
+echo "本文地址[https://github.com/daiwk/collections/blob/master/pdfs/collections.pdf](https://github.com/daiwk/collections/blob/master/pdfs/collections.pdf)" > collections.md
+
+cat ./posts/*.md | python3 trans_format.py >> ./collections.md
+
+cat ./collections.md | python3 ./change_for_pdf.py > collections-pdf.md
+pandoc -N -s --toc --smart --latex-engine=xelatex -V CJKmainfont='Heiti SC' -V mainfont='Times New Roman' -V geometry:margin=1in -f markdown+markdown_in_html_blocks+raw_html-implicit_figures ./collections-pdf.md -o ./pdfs/collections-all.pdf
 
 git add ./assets
 git add collections.md
-git add ./assets
 git add posts
 git add pdfs
+git add gen_pdfs.sh
 git commit -m 'x'
 git push
