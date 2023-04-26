7cd9e8dadb0e4945637eda648af5cfc575da8054
戴文恺 <daiwenkai@bytedance.com>
2023-04-26 17:34:43 +0800

    x

--- gen_pdfs.sh
+++ gen_pdfs.sh
@@ -1,4 +1,4 @@
-echo "本文地址[https://github.com/daiwk/collections/blob/master/pdfs/collections.pdf](https://github.com/daiwk/collections/blob/master/pdfs/collections.pdf)\n" > collections.md
+#echo "本文地址[https://github.com/daiwk/collections/blob/master/pdfs/collections.pdf](https://github.com/daiwk/collections/blob/master/pdfs/collections.pdf)\n" > collections.md
 
 cat ./posts/full.md | python3 trans_format.py >> ./collections-pdf.md
 
@@ -6,7 +6,6 @@ cat ./posts/full.md | python3 trans_format.py >> ./collections-pdf.md
 ##pandoc -N -s --toc --smart  --pdf-engine=xelatex -V CJKmainfont='Heiti SC' -V mainfont='Times New Roman' -V geometry:margin=1in -f markdown+markdown_in_html_blocks+raw_html-implicit_figures ./collections-pdf.md -o ./pdfs/collections.pdf
 
 git add ./assets
-git add collections.md
 git add posts
 git add pdfs
 git add gen_pdfs.sh
