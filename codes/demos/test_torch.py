4467c73e4562105f103de35908c37b6d654b1b9a
wk dai <wkdai@wkdeMacBook-Pro.lan>
2023-10-21 03:04:14 +0800

    x

--- codes/demos/test_torch.py
+++ codes/demos/test_torch.py
@@ -11,3 +11,15 @@ print(x[:, -2:])
 ##         [4, 5, 6]])
 ## tensor([[2, 3],
 ##         [5, 6]])
+
+import tensorflow as tf
+
+# TensorFlow中的inf
+inf_tf = tf.constant(float('inf'))
+x_inf_tf = tf.constant(float('-inf'))
+
+# PyTorch中的inf
+inf_torch = torch.tensor(float('inf'))
+x_inf_torch = torch.tensor(float('-inf'))
+
+print(inf_tf, x_inf_tf, inf_torch, x_inf_torch)
