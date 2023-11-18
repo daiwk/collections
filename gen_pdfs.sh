2017e2dbb5130210dcc4211cee6554b67b96b7a4
戴文恺 <daiwenkai@bytedance.com>
2023-11-19 01:57:25 +0800

    x

--- gen_pdfs.sh
+++ gen_pdfs.sh
@@ -1,6 +1,7 @@
 #echo "本文地址[https://github.com/daiwk/collections/blob/master/pdfs/collections.pdf](https://github.com/daiwk/collections/blob/master/pdfs/collections.pdf)\n" > collections.md
 
-cat ./posts/full.md | python3 trans_format.py >> ./pdfs/collections-pdf.md
+#cat ./posts/full.md | python3 trans_format.py >> ./pdfs/collections-pdf.md
+cat ./posts/full.md | python3 ./posts/change_format.py >> ./pdfs/collections-pdf.md
 cat ./posts/llm_aigc.md | python3 ./posts/change_format.py > ./pdfs/llm_aigc-pdf.md
 
 python3 gen_dot_sub.py
