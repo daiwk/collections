188057a08901ce54c5eec74fc262f983d79194ce
戴文恺 <daiwenkai@bytedance.com>
2023-11-19 01:53:02 +0800

    x

--- gen_pdfs.sh
+++ gen_pdfs.sh
@@ -1,17 +1,18 @@
 #echo "本文地址[https://github.com/daiwk/collections/blob/master/pdfs/collections.pdf](https://github.com/daiwk/collections/blob/master/pdfs/collections.pdf)\n" > collections.md
 
-cat ./posts/full.md | python3 trans_format.py >> ./collections-pdf.md
+cat ./posts/full.md | python3 trans_format.py >> ./pdfs/collections-pdf.md
 cat ./posts/llm_aigc.md | python3 ./posts/change_format.py > ./pdfs/llm_aigc-pdf.md
 
 python3 gen_dot_sub.py
 
-#pandoc -N -s --toc --smart --latex-engine=xelatex -V CJKmainfont='Heiti SC' -V mainfont='Times New Roman' -V geometry:margin=1in -f markdown+markdown_in_html_blocks+raw_html-implicit_figures ./collections-pdf.md -o ./pdfs/collections.pdf
 ##pandoc -N -s --toc --smart  --pdf-engine=xelatex -V CJKmainfont='Heiti SC' -V mainfont='Times New Roman' -V geometry:margin=1in -f markdown+markdown_in_html_blocks+raw_html-implicit_figures ./collections-pdf.md -o ./pdfs/collections.pdf
 
 cd pdfs
+pandoc -N -s --toc --toc-depth=5 --pdf-engine=xelatex -V CJKmainfont='Heiti SC' -V mainfont='Times New Roman' -V geometry:margin=1in -f markdown+markdown_in_html_blocks+smart+raw_html-implicit_figures ./collections-pdf.md -o ./collections.pdf
 pandoc -N -s --toc --toc-depth=5 --pdf-engine=xelatex -V CJKmainfont='Heiti SC' -V mainfont='Times New Roman' -V geometry:margin=1in -f markdown+markdown_in_html_blocks+smart+raw_html-implicit_figures --highlight-style tango ./llm_aigc-pdf.md -o ./llm_aigc.pdf
 cd -
 
+rm -rf pdfs/*.md
 
 git add ./assets
 git add posts
