ab89bbabc9bae3adef25a504bcde9e731e42a097
daiwk <daiwk1989@gmail.com>
2025-04-10 01:41:03 +0800

    x

--- publish/process.sh
+++ publish/process.sh
@@ -19,3 +19,4 @@ awk -F'\|' '{print $2}' ./${fname}.md | uniq |grep -v 论文
 func_summ llm_rec
 func efficient-cot
 
+pandoc -N -s --pdf-engine=xelatex --columns=30 --standalone --html-q-tags -V CJKmainfont='Heiti SC' -V mainfont='Times New Roman' -V geometry:margin=1in --metadata linkcolor=blue -f markdown+markdown_in_html_blocks+smart+raw_html-implicit_figures --highlight-style tango ./llm-rec-deep-research.md -o ./llm-rec-deep-research.pdf
