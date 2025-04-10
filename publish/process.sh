d46b5ad630e9ae0fe52e1e123b21e5a91773ff25
daiwk <daiwk1989@gmail.com>
2025-04-10 15:24:12 +0800

    x

--- publish/process.sh
+++ publish/process.sh
@@ -20,3 +20,4 @@ func_summ llm_rec
 func efficient-cot
 
 pandoc -N -s --pdf-engine=xelatex --columns=30 --standalone --html-q-tags -V CJKmainfont='Heiti SC' -V mainfont='Times New Roman' -V geometry:margin=1in --metadata linkcolor=blue -f markdown+markdown_in_html_blocks+smart+raw_html-implicit_figures --highlight-style tango ./llm-rec-deep-research.md -o ./llm-rec-deep-research.pdf
+pandoc -N -s --pdf-engine=xelatex --columns=30 --standalone --html-q-tags -V CJKmainfont='Heiti SC' -V mainfont='Times New Roman' -V geometry:margin=1in --metadata linkcolor=blue -f markdown+markdown_in_html_blocks+smart+raw_html-implicit_figures --highlight-style tango ./tmp.md -o ./tmp.pdf
